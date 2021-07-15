b6af96ccec285956b41f4c451cd52b0741d3c95a
cmkoge <charunak1@gmail.com>
2021-07-16 00:17:49 +0530

    # Added ffmpeg and fluent-ffmpeg # Add listner to send extract video from HTMl to electron # Add listner to catch video data from HTML

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const electorn = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electorn; // Access electorn overall running process
+const { app, BrowserWindow, ipcMain } = electorn; // Access electorn overall running process
 
 // Wait for ready
 app.on('ready', () => {
-    const mainWindow = new BrowserWindow({});
+    const mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true, // Enable default false nodeIntegration wich allows electron on HTMl
+            contextIsolation: false,
+        }
+    });
     mainWindow.loadFile("index.html") // Load html file
+});
+
+
+// Listen to the data send by event videoSubmit  
+ipcMain.on('videoSubmit', (e, path) => {
+    ffmpeg.ffprobe(path, (error, metadata) => {
+        console.log(metadata.format.duration);
+    });
 });
